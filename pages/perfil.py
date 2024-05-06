baf80114a835b5de477dfeb97e9651171ab98bb3
Marcelo <mclonso123@outlook.com>
2024-05-06 20:29:35 -0300

    Formatacao da foto e do perfil

--- pages/perfil.py
+++ pages/perfil.py
@@ -2,19 +2,24 @@ import streamlit as st
 import requests
 from urlback import URL
 
+espaco = r"$\hspace{2.5cm}$"
+
 with open( "font.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
-if 'user_access' in st.session_state:
-    if st.session_state.user != st.session_state.user_access:
-        st.switch_page('pages/usuario.py')
 if 'user_type' == 'restaurant':
     st.switch_page("page/menu.py")
 
 if 'user' in st.session_state:
     dados = requests.get(f'{URL}/usuarios/{st.session_state.user}').json()
 
-    st.title(dados['usuario']['nome'])
+    c1,c2,_ = st.columns(3)
+    c2.title(dados['usuario']['nome'])
+    c1.write("")
+    c1.image(dados['usuario']['foto_perfil'],width = 75)
+
+    st.write("")
+    st.write("")
 
     row1, row2 = st.columns([1, 1])
 
@@ -22,33 +27,39 @@ if 'user' in st.session_state:
         seguidores = dados['usuario']['seguidores']
         tamanho = len(dados['usuario']['seguidores'])
         if seguidores != []:
-            c = st.expander(label=f'{tamanho} seguidores:')
+            c = st.expander(label=f'{espaco}{tamanho} seguidores:')
             with c:
                 for seguidor in dados['usuario']['seguidores']:
                     if st.button(f"{seguidor['nome']}", use_container_width=True, key=f"seguidor{seguidor['_id']}"):
                         st.session_state.user_access = seguidor['_id']
                         st.rerun()
         else:
-            st.write('0 seguidores')
+            st.write(f'{espaco}0 seguidores')
 
 
     with row2:
         seguindo = dados['usuario']['seguindo']
         tamanho = len(dados['usuario']['seguindo'])
         if seguindo != []:
-            c = st.expander(label=f'{tamanho} seguindo:')
+            c = st.expander(label=f'{espaco}{tamanho} seguindo:')
             with c:
                 for seguindo in dados['usuario']['seguindo']:
                     if st.button(f"{seguindo['nome']}", use_container_width=True, key=f"seguindo{seguindo['_id']}"):
                         st.session_state.user_access = seguindo['_id']
                         st.rerun()
         else:
-            st.write('0 seguindo')
+            st.write(f'{espaco}0 seguindo')
 
+    st.write("")
+    st.write("")
+    st.write("")
     st.subheader('Restaurantes favoritos:')
     for item in dados['usuario']['rest_fav']:
         st.write(f"Nome: {item['nome']}")
 
+    st.write("")
+    st.write("")
+    st.write("")
     st.subheader('Comida favorita:')
     for item in dados['usuario']['comida_fav']:
         st.write(f"Nome: {item}")
