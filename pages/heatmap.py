2124559df9b0ba3a543014bbd731a8fd4d8f4656
Marcelo <mclonso123@outlook.com>
2024-05-07 01:21:56 -0300

    Filtro e heatmap no jeitinho

--- pages/heatmap.py
+++ pages/heatmap.py
@@ -4,6 +4,11 @@ from urlback import URL
 import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
+from used_func import header
+
+with open( "font.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+
 
 if 'center' not in st.session_state:
     st.session_state['center'] = [-23.588609, -46.681847]
@@ -13,7 +18,11 @@ if 'heatmap' not in st.session_state:
 
 data = requests.get(f'{URL}/usuarios/loc').json() if st.session_state.heatmap[0] == 'all' else requests.get(f'{URL}/usuario/{st.session_state.heatmap[1]}/loc').json()
 
-m = folium.Map(location=st.session_state.center, zoom_start=4)  # Coordenadas do centro dos EUA
+m = folium.Map(location=st.session_state.center, zoom_start=13)  # Coordenadas do centro dos EUA
 HeatMap(data).add_to(m)
 
+
+header(False, False)
+
+st.subheader("Heatmap")
 st_folium(m, width=725)
